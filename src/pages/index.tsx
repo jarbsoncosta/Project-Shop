674fbc0ad064e9b7056f0f5c666aec7399c107e6
Jarbson Costa <jarbsonfc@gmail.com>
2022-12-01 10:23:57 -0300

    feat: create styles global

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import {styled} from '../styles'
 
+const Button = styled('button', {
+  backgroundColor: '$green500',
 
+  borderRadius: 4,
+  padding:"1rem"
+  
+  
+})
 export default function Home() {
   return (
-   <h1>Hello World</h1>
+   <Button>Enviar</Button>
   )
 }
