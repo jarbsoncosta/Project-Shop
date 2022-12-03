55084509d07843438bf6c699a7e18c4ad6d2fb00
Jarbson Costa <jarbsonfc@gmail.com>
2022-12-03 11:53:05 -0300

    feat: styling home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,28 @@
-import {styled} from '../styles'
+import Image from 'next/image'
+import { styled } from '../styles'
+import { Footer, HomeContainer, Product } from '../styles/pages/home'
+import Shirt1 from '../assets/camisetas/shirt-1.png'
+import Shirt2 from '../assets/camisetas/shirt-2.png'
+import Shirt3 from '../assets/camisetas/shirt-3.png'
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
 
-  borderRadius: 4,
-  padding:"1rem"
-  
-  
-})
 export default function Home() {
   return (
-   <Button>Enviar</Button>
+    <HomeContainer>
+      <Product>
+        <Image src={Shirt1} alt="" width={520} height={480} />
+        <Footer>
+          <strong>Camiseta Beyond the Limits</strong>
+          <span>R$ 79,90</span>
+        </Footer>
+      </Product>
+      <Product>
+        <Image src={Shirt3} alt="" width={520} height={480} />
+        <Footer>
+          <strong>Camiseta Beyond the Limits</strong>
+          <span>R$ 79,90</span>
+        </Footer>
+      </Product>
+    </HomeContainer>
   )
 }
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,67 @@
+import { styled } from "..";
+
+export const HomeContainer = styled('main',{
+display:"flex",
+gap:"3rem",
+width:"100%",
+maxWidth:'calc(100vw - ((100vw - 1180px) / 2))',
+marginLeft: "auto",
+padding:'1.5rem'
+
+})
+
+export const Product = styled('a',{
+    background:'linear-gradient(180deg, #1EA483 0%, #7465D4 100%)',
+    borderRadius:6,
+    padding: 3,
+    cursor:"pointer",
+
+    position:"relative",
+    overflow:'hidden',
+
+    img:{
+      objectFit:"cover"
+    },
+    
+    '&:hover':{
+      footer:{
+       transform: 'translateY(0%)',
+       opacity:1
+      }
+   
+     }
+    
+})
+
+export const Footer = styled('footer',{
+  display:"flex", 
+  justifyContent: "space-between",
+  padding:"2rem",
+  background:'rgb(0, 0, 0, 0.6)',
+  borderRadius:6,
+  alignItems:"center",
+
+  transform:'translateY(110%)',
+  opacity:0,
+  transition:'all 0.2s ease-in-out',
+ 
+  img:{
+    objectFit:"cover"
+  },
+  
+  strong:{
+    color:"$gray100",
+    fontSize:"1.25rem",
+    fontWeight:"bold"
+
+  },
+  span:{
+    color:"$green300",
+    fontSize:"1.5rem",
+    fontWeight:"bold"
+
+  },
+
+
+    
+})
